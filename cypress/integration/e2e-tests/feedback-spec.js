10c7b13d50b78c09fa52d0e8161b2fb025db29bf
mikec95 <mikecarbonari1@gmail.com>
2021-04-15 15:06:38 -0500

    Move base variable to parent level. Make constant. Include assertions for form loading

--- cypress/integration/e2e-tests/feedback-spec.js
+++ cypress/integration/e2e-tests/feedback-spec.js
@@ -1,9 +1,13 @@
 describe('Feedback Test', () => {
+  const BASE = Cypress.config('baseUrl')
   before(() => {
-    const base = Cypress.config('baseUrl')
-    cy.visit(base)
+    cy.visit(BASE)
+    cy.get('li#feedback').click()
   })
-  it('Form Loaded', () => {})
+  it('Form Loaded', () => {
+    cy.url().should('eq', BASE + '/feedback.html')
+    cy.get('form').should('be.visible')
+  })
   it('Form Data Entered', () => {})
   it('Form Submitted', () => {})
   it('Feedback Message Displayed', () => {})
