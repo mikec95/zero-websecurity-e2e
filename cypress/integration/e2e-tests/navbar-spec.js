2b8bad17b9b8c0089f29a5e719acc1793928b293
mikec95 <mikecarbonari1@gmail.com>
2021-04-15 14:52:58 -0500

    Add e2e tests for navbar

--- cypress/integration/e2e-tests/navbar-spec.js
+++ cypress/integration/e2e-tests/navbar-spec.js
@@ -1,3 +1,21 @@
 describe('Navbar Test', () => {
-  it('', () => {})
+  const BASE = Cypress.config('baseUrl')
+  before(() => {
+    cy.visit(BASE)
+  })
+  it('Home', () => {
+    cy.get('#homeMenu').click()
+    cy.url().should('eq', BASE + '/index.html')
+    cy.get('#carousel').should('be.visible')
+  })
+  it('Online Banking', () => {
+    cy.get('#onlineBankingMenu').click()
+    cy.url().should('eq', BASE + '/online-banking.html')
+    cy.get('div.hero-unit').find('h1').should('have.text', 'Online Banking')
+  })
+  it('Feedback', () => {
+    cy.get('#feedback').click()
+    cy.url().should('eq', BASE + '/feedback.html')
+    cy.get('h3').should('have.text', 'Feedback')
+  })
 })
