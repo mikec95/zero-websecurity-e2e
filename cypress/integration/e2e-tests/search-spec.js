dc390ca7171710795aaa9f1927de62cc8743b81d
mikec95 <mikecarbonari1@gmail.com>
2021-04-15 14:56:04 -0500

    Pass baseUrl from cypress.json into visit function in test. Update assertion

--- cypress/integration/e2e-tests/search-spec.js
+++ cypress/integration/e2e-tests/search-spec.js
@@ -1,12 +1,13 @@
 describe('Searchbox Test', () => {
   before(() => {
-    cy.visit('http://zero.webappsecurity.com/')
+    const base = Cypress.config('baseUrl')
+    cy.visit(base)
   })
   it('Should type into Searchbox and press ENTER on keyboard', () => {
     cy.get('#searchTerm').as('search')
     cy.get('@search').type('Search Test {Enter}')
   })
   it('Should show Search Results page', () => {
-    cy.get('h2').contains('Search Results:')
+    cy.get('h2').should('have.text', 'Search Results:')
   })
 })
