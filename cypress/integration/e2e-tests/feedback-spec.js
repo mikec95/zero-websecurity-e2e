759af7d234ebc0192ddd65a1a7b1c21f80353d61
mikec95 <mikecarbonari1@gmail.com>
2021-04-15 15:01:41 -0500

    Create javascript file to house feedback form test

---
+++ cypress/integration/e2e-tests/feedback-spec.js
@@ -0,0 +1,10 @@
+describe('Feedback Test', () => {
+  before(() => {
+    const base = Cypress.config('baseUrl')
+    cy.visit(base)
+  })
+  it('Form Loaded', () => {})
+  it('Form Data Entered', () => {})
+  it('Form Submitted', () => {})
+  it('Feedback Message Displayed', () => {})
+})
