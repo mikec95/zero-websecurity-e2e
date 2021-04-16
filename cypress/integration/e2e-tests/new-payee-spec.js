45fdd0cd204fea37eac1a68bdaf5b538b3bcd556
mikec95 <mikecarbonari1@gmail.com>
2021-04-16 15:00:24 -0500

    Add login functionality to before method. CUse fixture to set login data

--- cypress/integration/e2e-tests/new-payee-spec.js
+++ cypress/integration/e2e-tests/new-payee-spec.js
@@ -1,7 +1,19 @@
 describe('New Payee Creation Test', () => {
   const BASE = Cypress.config('baseUrl')
   before(() => {
-    cy.visit(BASE)
+    cy.visit(BASE + '/login.html')
+    cy.get('#login_form').should('be.visible')
+
+    cy.fixture('user').then(user => {
+      const username = user.id
+      const pass = user.pass
+      cy.get('#user_login').type(username)
+      cy.get('#user_password').type(pass)
+    })
+    cy.get('#user_remember_me').click()
+    cy.get('[type="submit"]').click()
+
+    cy.get('ul.nav-tabs').should('be.visible')
   })
   it('', () => {})
 })
